6fde03ee5fa1978cc80f161854d39d1465ba2b7a
ze020829 <132757037+ze020829@users.noreply.github.com>
2024-05-30 16:07:42 +0800

    修改类名

--- src/main/java/com/example/nftmarket/entity/DinosaurEgg.java
+++ src/main/java/com/example/nftmarket/entity/DinosaurEgg.java
@@ -6,8 +6,8 @@ import lombok.Data;
  * @Params:龙蛋ID，龙父哈希值，龙母哈希值，是否孵化中
  **/
 @Data
-public class DinosaurAgg {
-    Integer AggId;
+public class DinosaurEgg {
+    Integer eggId;
     String DinosaurFather;
     String DinosaurMother;
     boolean isHatched;
