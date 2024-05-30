c733380925fbaae0b0c60c2c89922575be6f583b
FBCD <745098947@qq.com>
2024-05-30 15:47:03 +0800

    修改了基准定价的公式

--- src/main/java/com/example/nftmarket/utils/PriceUtils.java
+++ src/main/java/com/example/nftmarket/utils/PriceUtils.java
@@ -3,7 +3,7 @@ package com.example.nftmarket.utils;
 
 
 //根据恐龙的稀有度以及恐龙的颜色来进行搭配操作
-//平台收取的手续费+(NFT稀有度*颜色权重*统一时段售卖的数量)=最终的NFT基准定价
+//平台收取的手续费+(NFT稀有度*颜色权重/统一时段售卖的数量)=最终的NFT基准定价
 public class PriceUtils {
     private static final RarityUtils.Rarities[] Rarites = RarityUtils.Rarities.values();
     private static final DinosaurDetails.Color[] colors=DinosaurDetails.Color.values();
